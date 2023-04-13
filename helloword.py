36f7e495ff40ebbbac94426f9e043e1238d3d378
buxexudo1 <gustavoclaudino.senai@gmail.com>
2023-04-13 14:27:30 -0300

    first commit

---
+++ helloword.py
@@ -0,0 +1,16 @@
+print ("hello.word")
+
+ano_nascimento = int(input("Digite o ano que nasceu: "))
+
+ano_atual = int(input("Ano que atual: "))
+
+idade = ano_atual - ano_nascimento
+
+niver = int(input("você ja fez aniversário? digite 1 para sim 2 para não:"))
+if niver == 1: 
+     idade - 1
+else :
+    print(idade)
+
+print("Sua idade é :", idade,"anos")
+
